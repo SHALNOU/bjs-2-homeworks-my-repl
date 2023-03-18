456a639c1df34573989d205fe8d98d38b88e2f17
vladislav.Baiborodov <baiborodov333@mail.ru>
2023-03-18 17:01:02 +0500

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,58 +1,53 @@
-"use strict"
-
-
+"use strict";
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let descriminant = b ** 2 - 4 * a * c;
-	let descriminantOne = b ** 2 - 4 * a * c;
-	if (descriminant === 0) {
-		descriminant = -b / (2 * a);
-		arr.push(descriminant);
-	} else if (descriminant > 0) {
-		let result;
-		descriminant = (-b + Math.sqrt(descriminant)) / (2 * a);
-		result = descriminant;
-		arr.push(result);
-		let resultOne;
-		descriminantOne = (-b - Math.sqrt(descriminantOne)) / (2 * a);
-		resultOne = Number(descriminantOne);
-		arr.push(resultOne);
-	} else {
-		return [];
-	}
+  let arr = [];
+  let descriminant = b ** 2 - 4 * a * c;
+  let descriminantOne = b ** 2 - 4 * a * c;
+  if (descriminant === 0) {
+    descriminant = -b / (2 * a);
+    arr.push(descriminant);
+  } else if (descriminant > 0) {
+    let result;
+    descriminant = (-b + Math.sqrt(descriminant)) / (2 * a);
+    result = descriminant;
+    arr.push(result);
+    let resultOne;
+    descriminantOne = (-b - Math.sqrt(descriminantOne)) / (2 * a);
+    resultOne = Number(descriminantOne);
+    arr.push(resultOne);
+  } else {
+    return [];
+  }
 
-
-	return arr;
+  return arr;
 }
 
 solveEquation();
 
-
-
-
 //процентную ставку,procent сумму первоначального взноса,contribution сумму кредита amount, и срок (длительность кредита в месяцах)countMonths
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent)) {
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
+  }
 
-	if (isNaN(percent)) {
-		return false;
-	} else if (isNaN(contribution)) {
-		return false;
-	} else if (isNaN(amount)) {
-		return false;
-	}
+  let monthlyPercentage = percent / 100 / 12;
 
-	let monthlyPercentage = percent / 100 / 12;
+  let teloCredit = amount - contribution;
 
-	let teloCredit = amount - contribution;
+  let loanPayment =
+    teloCredit *
+    (monthlyPercentage +
+      monthlyPercentage / ((1 + monthlyPercentage) ** countMonths - 1));
 
-	let loanPayment = teloCredit * (monthlyPercentage + (monthlyPercentage / (((1 + monthlyPercentage) ** countMonths) - 1)));
+  const totalAmount = loanPayment * countMonths;
 
-	const totalAmount = loanPayment * countMonths;
-
-	return Number(totalAmount.toFixed(2));
+  return Number(totalAmount.toFixed(2));
 }
 
 calculateTotalMortgage();
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,139 @@
+// function getArrayParams(...arr) {
+
+// 	let min = Math.min(...arr);
+// 	let max = Math.max(...arr);
+// 	const sum = arr.reduce((acc, curr) => acc + curr, 0);
+// 	const avg = sum / arr.length;
+
+// 	console.log(avg);
+
+// 	return { min: min, max: max, avg: +avg.toFixed(2) };
+// }
+
+// console.log(getArrayParams(1, 2, 3, -100, 10));
+
 function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		sum += arr[i];
+	}
+
+	const fix = sum / arr.length;
+	const avg = Number(fix.toFixed(2));
+
+	return { min, max, avg };
 }
 
-function summElementsWorker(...arr) {
+console.log(getArrayParams(-99, 99, 10));
+
+
+
+
+
+function summElementsWorker(...arr)//суммирования элементов; 
+{
+	return arr.reduce((acc, val) => acc + val, 0);
+
+}
+console.log(summElementsWorker(1, 4, 4, 4,));//суммир элементов масиыва
+
+
+
+
+function differenceMaxMinWorker(...arr)//вычисления разницы максимального и минимального элементов; 
+{
+	if (arr.length === 0) {
+		return 0;
+	}
+
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
+	let result = max - min;
+	return result;
+}
+
+console.log(differenceMaxMinWorker(2, 5, 6, 6, 8, 3, 5));
+
+
+
+
+
+function differenceEvenOddWorker(...arr) //вычисления разницы сумм чётных и нечётных элементов;
+{
+
+	if (arr.length === 0) {
+		return 0;
+	}
+
+	let maxOddWorkers = 0;
+	let minOddWorkers = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr[i] % 2 === 0) {
+			maxOddWorkers += arr[i];
+		} else if (arr[i] % 2 !== 0) {
+			minOddWorkers += arr[i];
+		}
+	}
+
+	console.log(maxOddWorkers);
+	console.log(minOddWorkers);
+	let result = maxOddWorkers - minOddWorkers;
+	return result;
+}
+
+console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
+
+
+
+function averageEvenElementsWorker(...arr) //вычисления среднего значения чётных элементов.
+{
+
+	let umEvenElement = 0;
+	let countEvenElement = 0;
+
+	if (arr.length === 0) {
+		return 0;
+	}
+
+	for (let i = 0; i < arr.length; i++) {
+
+		if (arr[i] % 2 === 0) {
+			countEvenElement++;
+			umEvenElement += arr[i];
+		}
+	}
+
+
+	let result = umEvenElement / countEvenElement;
+
+	return result;
 
 }
 
-function differenceMaxMinWorker(...arr) {
+averageEvenElementsWorker();
 
+
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		}
+	}
+	return maxWorkerResult;
 }
 
-function differenceEvenOddWorker(...arr) {
+makeWork();
 
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
