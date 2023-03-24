4673499b93e6e4718768d1ad282b6aeedd4da618
vladislav.Baiborodov <baiborodov333@mail.ru>
2023-03-24 17:17:31 +0500

    finish-array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((el, index) => el === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	const filtr = users.filter(user => user.gender === gender);
+
+	if (filtr.length === 0) return 0;
+
+	const ageFilter = filtr.reduce((acc, user) => acc + user.age, 0);
+
+	const result = ageFilter / filtr.length;
+
+	return result;
 }
